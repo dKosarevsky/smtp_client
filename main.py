3ab570616e0f3c8e2751e3a8cb0b5adb3315d441
kosarevskiydp <kosarevskiydp@pik.ru>
2022-03-27 21:16:35 +0300

    upd lab_5

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import smtplib
+import ssl
+
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import formatdate
 
+from PIL import Image
+from io import BytesIO
+
 # st.set_page_config(initial_sidebar_state="collapsed")
-st.sidebar.image('logo.png', width=300)
-SMTP_HOST = ["smtp.yandex.ru", 465]
+st.sidebar.image("logo.png", width=300)
+SMTP_HOST = ["smtp.yandex.com", 465]
+FILE_TYPES = ["png", "bmp", "jpg", "jpeg"]
 
 
 def header():
@@ -23,29 +29,47 @@ def header():
     st.sidebar.markdown(author)
 
 
-def fill_mime(from_address, to_address, msg_subject, msg_text, filenames=None):
-    """
-        Заполнение полей письма
-    """
+def uploader(file):
+    show_file = st.empty()
+    if not file:
+        show_file.info("valid file extension: " + ", ".join(FILE_TYPES))
+        return False
+    return file
+
+
+def image_to_byte_array(image: Image):
+    img_byte_arr = BytesIO()
+    image.save(img_byte_arr, format=image.format)
+    img_byte_arr = img_byte_arr.getvalue()
+    return img_byte_arr
+
+
+def attach_files_to_mail(mime, file):
+    filename = file.name
+    img = Image.open(file)
+    img = image_to_byte_array(img)
+
+    part = MIMEApplication(img, Name=filename)
+    part["Content-Disposition"] = f"attachment; filename={filename}"
+    mime.attach(part)
+
+
+def fill_mail(from_address, to_address, msg_subject, msg_text, file=None):
     mime = MIMEMultipart()
-    mime['From'] = from_address
-    mime['To'] = to_address
-    mime['Date'] = formatdate(localtime=True)
-    mime['Subject'] = msg_subject
-    mime.attach(MIMEText(msg_text, 'plain'))
-    # attach_files_to_mime(mime, filenames)
+    mime["From"] = from_address
+    mime["To"] = to_address
+    mime["Date"] = formatdate(localtime=True)
+    mime["Subject"] = msg_subject
+    mime.attach(MIMEText(msg_text, "plain"))
+    if file:
+        attach_files_to_mail(mime, file)
     return mime
 
 
-def send_mime_mail(mime, smtp_host, from_address, password):
-    """
-        Отправка письма с вложениями
-    """
-    server = smtplib.SMTP(smtp_host[0], smtp_host[1])
-    server.starttls()
-    server.login(from_address, password)
-    server.sendmail(mime['From'], mime['To'], mime.as_string())
-    server.quit()
+def send_mail(mime, smtp_host, password, context):
+    with smtplib.SMTP_SSL(smtp_host[0], smtp_host[1], context=context) as server:
+        server.login(mime["From"], password)
+        server.sendmail(mime["From"], mime["To"], mime.as_string())
 
 
 def main():
@@ -57,7 +81,7 @@ def main():
     with st.form("send_mail"):
         c1, c2 = st.columns(2)
         from_ = c1.text_input("Введите email отправителя:", value="example@ya.ru")
-        to = c2.text_input("Введите email получателя:", value="example@ya.ru")
+        to = c2.text_input("Введите email получателя:", value="if@kosarevsky.ru")
         c3, c4 = st.columns(2)
         pwd = c3.text_input("Введите пароль:", type="password")
         subj = c4.text_input("Введите тему сообщения:", value="Привет")
@@ -65,11 +89,13 @@ def main():
             label="Введите сообщение:",
             value="Самые ужасные строения — это те, бюджет которых был слишком велик для поставленных целей."
         )
+        file = uploader(st.file_uploader("Загрузите файл:", type=FILE_TYPES))
 
-        if st.form_submit_button("Отправить сообщение"):
-            # key_filenames = find_files_with_keyword(KEY)
-            mime = fill_mime(from_, to, subj, message, filenames=None)
-            send_mime_mail(mime, SMTP_HOST, from_, pwd)
+        submitted = st.form_submit_button("Отправить сообщение")
+        if submitted:
+            context = ssl.create_default_context()
+            mime = fill_mail(from_, to, subj, message, file=file if file else None)
+            send_mail(mime, SMTP_HOST, pwd, context)
 
 
 if __name__ == "__main__":
