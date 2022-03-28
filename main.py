2830f5457ae5a41a9753f07d748f11ff936c0486
kosarevskiydp <kosarevskiydp@pik.ru>
2022-03-28 07:51:28 +0300

    upd lab_5

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import smtplib
 import ssl
+import re
 
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
@@ -13,6 +14,8 @@ from io import BytesIO
 # st.set_page_config(initial_sidebar_state="collapsed")
 st.sidebar.image("logo.png", width=300)
 SMTP_HOST = ["smtp.yandex.com", 465]
+FROM = "bmstu.networks@ya.ru"
+PWD = st.secrets["PWD"]
 FILE_TYPES = ["png", "bmp", "jpg", "jpeg"]
 
 
@@ -76,26 +79,31 @@ def main():
     header()
     st.markdown("### Лабораторная работа №5")
     st.markdown("**Тема:** Реализация SMTP-клиента")
+    st.markdown("SMTP-клиент реализован на базе сервера почтовых сообщений Yandex")
     st.markdown("---")
 
     with st.form("send_mail"):
         c1, c2 = st.columns(2)
-        from_ = c1.text_input("Введите email отправителя:", value="example@ya.ru")
-        to = c2.text_input("Введите email получателя:", value="if@kosarevsky.ru")
-        c3, c4 = st.columns(2)
-        pwd = c3.text_input("Введите пароль:", type="password")
-        subj = c4.text_input("Введите тему сообщения:", value="Привет")
+        email = c1.text_input("Введите email получателя:", value="if@kosarevsky.ru")
+        subj = c2.text_input("Введите тему сообщения:", value="Привет")
         message = st.text_area(
             label="Введите сообщение:",
-            value="Самые ужасные строения — это те, бюджет которых был слишком велик для поставленных целей."
+            value="Если проект не укладывается в сроки, то добавление рабочей силы задержит его еще больше."
         )
         file = uploader(st.file_uploader("Загрузите файл:", type=FILE_TYPES))
 
         submitted = st.form_submit_button("Отправить сообщение")
         if submitted:
-            context = ssl.create_default_context()
-            mime = fill_mail(from_, to, subj, message, file=file if file else None)
-            send_mail(mime, SMTP_HOST, pwd, context)
+            if re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
+                try:
+                    context = ssl.create_default_context()
+                    mime = fill_mail(FROM, email, subj, message, file=file if file else None)
+                    send_mail(mime, SMTP_HOST, PWD, context)
+                    st.code("Сообщение отправлено.")
+                except Exception as e:
+                    st.error(e)
+            else:
+                st.error("Необходимо указать корректный email получателя.")
 
 
 if __name__ == "__main__":
